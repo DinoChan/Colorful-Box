8f1d99219f94f61a75d641ae42423440a0b175ad
dino.chan <dino.chan@live.com>
2017-07-20 13:51:21 +0800

    increase performance

--- ColorfulBox/ColorfulBox/ArgbColorBridge.cs
+++ ColorfulBox/ColorfulBox/ArgbColorBridge.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ using Windows.UI.Xaml;
 
 namespace ColorfulBox
 {
-    public class ArgbColorBridge : DependencyObject
+    public class ArgbColorBridge : DependencyObject,INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _isIgnoreColorChanged;
 
         /// <summary>
         /// 获取或设置Color的值
@@ -37,136 +41,243 @@ namespace ColorfulBox
 
         protected virtual void OnColorChanged(Color oldValue, Color newValue)
         {
-            A = newValue.A;
-            R = newValue.R;
-            G = newValue.G;
-            B = newValue.B;
+
+
+            _isIgnoreColorChanged = true;
+            try
+            {
+                if (A != newValue.A)
+                    A = newValue.A;
+
+                if (R != newValue.R)
+                    R = newValue.R;
+
+                if (G != newValue.G)
+                    G = newValue.G;
+
+                if (B != newValue.B)
+                    B = newValue.B;
+            }
+            finally
+            {
+                _isIgnoreColorChanged = false;
+            }
         }
 
 
+        private int _a;
 
         /// <summary>
-        /// 获取或设置A的值
-        /// </summary>  
+        /// 获取或设置 A 的值
+        /// </summary>
         public int A
         {
-            get { return (int)GetValue(AProperty); }
-            set { SetValue(AProperty, value); }
+            get { return _a; }
+            set
+            {
+                if (_a == value)
+                    return;
+
+                _a = value;
+                OnPropertyChanged("A");
+                OnArgbChanged();
+            }
         }
 
+
+        private int _r;
+
         /// <summary>
-        /// 标识 A 依赖属性。
+        /// 获取或设置 R 的值
         /// </summary>
-        public static readonly DependencyProperty AProperty =
-            DependencyProperty.Register("A", typeof(int), typeof(ArgbColorBridge), new PropertyMetadata(0, OnAChanged));
-
-        private static void OnAChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
-        {
-            ArgbColorBridge target = obj as ArgbColorBridge;
-            int oldValue = (int)args.OldValue;
-            int newValue = (int)args.NewValue;
-            if (oldValue != newValue)
-                target.OnAChanged(oldValue, newValue);
-        }
-
-        protected virtual void OnAChanged(int oldValue, int newValue)
-        {
-            OnArgbChanged();
-        }
-
-
-        /// <summary>
-        /// 获取或设置R的值
-        /// </summary>  
         public int R
         {
-            get { return (int)GetValue(RProperty); }
-            set { SetValue(RProperty, value); }
+            get { return _r; }
+            set
+            {
+                if (_r == value)
+                    return;
+
+                _r = value;
+                OnPropertyChanged("R");
+                OnArgbChanged();
+            }
         }
 
+
+        private int _g;
+
         /// <summary>
-        /// 标识 R 依赖属性。
+        /// 获取或设置 G 的值
         /// </summary>
-        public static readonly DependencyProperty RProperty =
-            DependencyProperty.Register("R", typeof(int), typeof(ArgbColorBridge), new PropertyMetadata(0, OnRChanged));
-
-        private static void OnRChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
-        {
-            ArgbColorBridge target = obj as ArgbColorBridge;
-            int oldValue = (int)args.OldValue;
-            int newValue = (int)args.NewValue;
-            if (oldValue != newValue)
-                target.OnRChanged(oldValue, newValue);
-        }
-
-        protected virtual void OnRChanged(int oldValue, int newValue)
-        {
-            OnArgbChanged();
-        }
-
-
-        /// <summary>
-        /// 获取或设置G的值
-        /// </summary>  
         public int G
         {
-            get { return (int)GetValue(GProperty); }
-            set { SetValue(GProperty, value); }
+            get { return _g; }
+            set
+            {
+                if (_g == value)
+                    return;
+
+                _g = value;
+                OnPropertyChanged("G");
+                OnArgbChanged();
+            }
         }
 
+
+        private int _b;
+
         /// <summary>
-        /// 标识 G 依赖属性。
+        /// 获取或设置 B 的值
         /// </summary>
-        public static readonly DependencyProperty GProperty =
-            DependencyProperty.Register("G", typeof(int), typeof(ArgbColorBridge), new PropertyMetadata(0, OnGChanged));
-
-        private static void OnGChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
-        {
-            ArgbColorBridge target = obj as ArgbColorBridge;
-            int oldValue = (int)args.OldValue;
-            int newValue = (int)args.NewValue;
-            if (oldValue != newValue)
-                target.OnGChanged(oldValue, newValue);
-        }
-
-        protected virtual void OnGChanged(int oldValue, int newValue)
-        {
-            OnArgbChanged();
-        }
-
-
-        /// <summary>
-        /// 获取或设置B的值
-        /// </summary>  
         public int B
         {
-            get { return (int)GetValue(BProperty); }
-            set { SetValue(BProperty, value); }
+            get { return _b; }
+            set
+            {
+                if (_b == value)
+                    return;
+
+                _b = value;
+                OnPropertyChanged("B");
+                OnArgbChanged();
+            }
         }
 
-        /// <summary>
-        /// 标识 B 依赖属性。
-        /// </summary>
-        public static readonly DependencyProperty BProperty =
-            DependencyProperty.Register("B", typeof(int), typeof(ArgbColorBridge), new PropertyMetadata(0, OnBChanged));
-
-        private static void OnBChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        private void OnPropertyChanged(string propertyName)
         {
-            ArgbColorBridge target = obj as ArgbColorBridge;
-            int oldValue = (int)args.OldValue;
-            int newValue = (int)args.NewValue;
-            if (oldValue != newValue)
-                target.OnBChanged(oldValue, newValue);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        protected virtual void OnBChanged(int oldValue, int newValue)
-        {
-            OnArgbChanged();
-        }
+        ///// <summary>
+        ///// 获取或设置A的值
+        ///// </summary>  
+        //public int A
+        //{
+        //    get { return (int)GetValue(AProperty); }
+        //    set { SetValue(AProperty, value); }
+        //}
+
+        ///// <summary>
+        ///// 标识 A 依赖属性。
+        ///// </summary>
+        //public static readonly DependencyProperty AProperty =
+        //    DependencyProperty.Register("A", typeof(int), typeof(ArgbColorBridge), new PropertyMetadata(0, OnAChanged));
+
+        //private static void OnAChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        //{
+        //    ArgbColorBridge target = obj as ArgbColorBridge;
+        //    int oldValue = (int)args.OldValue;
+        //    int newValue = (int)args.NewValue;
+        //    if (oldValue != newValue)
+        //        target.OnAChanged(oldValue, newValue);
+        //}
+
+        //protected virtual void OnAChanged(int oldValue, int newValue)
+        //{
+        //    OnArgbChanged();
+        //}
+
+
+        ///// <summary>
+        ///// 获取或设置R的值
+        ///// </summary>  
+        //public int R
+        //{
+        //    get { return (int)GetValue(RProperty); }
+        //    set { SetValue(RProperty, value); }
+        //}
+
+        ///// <summary>
+        ///// 标识 R 依赖属性。
+        ///// </summary>
+        //public static readonly DependencyProperty RProperty =
+        //    DependencyProperty.Register("R", typeof(int), typeof(ArgbColorBridge), new PropertyMetadata(0, OnRChanged));
+
+        //private static void OnRChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        //{
+        //    ArgbColorBridge target = obj as ArgbColorBridge;
+        //    int oldValue = (int)args.OldValue;
+        //    int newValue = (int)args.NewValue;
+        //    if (oldValue != newValue)
+        //        target.OnRChanged(oldValue, newValue);
+        //}
+
+        //protected virtual void OnRChanged(int oldValue, int newValue)
+        //{
+        //    OnArgbChanged();
+        //}
+
+
+        ///// <summary>
+        ///// 获取或设置G的值
+        ///// </summary>  
+        //public int G
+        //{
+        //    get { return (int)GetValue(GProperty); }
+        //    set { SetValue(GProperty, value); }
+        //}
+
+        ///// <summary>
+        ///// 标识 G 依赖属性。
+        ///// </summary>
+        //public static readonly DependencyProperty GProperty =
+        //    DependencyProperty.Register("G", typeof(int), typeof(ArgbColorBridge), new PropertyMetadata(0, OnGChanged));
+
+        //private static void OnGChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        //{
+        //    ArgbColorBridge target = obj as ArgbColorBridge;
+        //    int oldValue = (int)args.OldValue;
+        //    int newValue = (int)args.NewValue;
+        //    if (oldValue != newValue)
+        //        target.OnGChanged(oldValue, newValue);
+        //}
+
+        //protected virtual void OnGChanged(int oldValue, int newValue)
+        //{
+        //    OnArgbChanged();
+        //}
+
+
+        ///// <summary>
+        ///// 获取或设置B的值
+        ///// </summary>  
+        //public int B
+        //{
+        //    get { return (int)GetValue(BProperty); }
+        //    set { SetValue(BProperty, value); }
+        //}
+
+        ///// <summary>
+        ///// 标识 B 依赖属性。
+        ///// </summary>
+        //public static readonly DependencyProperty BProperty =
+        //    DependencyProperty.Register("B", typeof(int), typeof(ArgbColorBridge), new PropertyMetadata(0, OnBChanged));
+
+       
+
+        //private static void OnBChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        //{
+        //    ArgbColorBridge target = obj as ArgbColorBridge;
+        //    int oldValue = (int)args.OldValue;
+        //    int newValue = (int)args.NewValue;
+        //    if (oldValue != newValue)
+        //        target.OnBChanged(oldValue, newValue);
+        //}
+
+        //protected virtual void OnBChanged(int oldValue, int newValue)
+        //{
+        //    OnArgbChanged();
+        //}
 
         private void OnArgbChanged()
         {
+            if (_isIgnoreColorChanged)
+                return;
+
             Color = Color.FromArgb(Convert.ToByte(A), Convert.ToByte(R), Convert.ToByte(G), Convert.ToByte(B));
         }
+
+
     }
 }
