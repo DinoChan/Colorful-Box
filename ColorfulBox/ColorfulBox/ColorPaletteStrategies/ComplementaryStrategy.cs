40fc38b3fe65dff0ef5a462c383aca978c48f396
Dino Chan <dino.chan@live.com>
2017-09-01 17:22:42 +0800

    sync code

---
+++ ColorfulBox/ColorfulBox/ColorPaletteStrategies/ComplementaryStrategy.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Uwp.Helpers;
+using Windows.UI;
+
+namespace ColorfulBox
+{
+    public class ComplementaryStrategy : ColorPaletteStrategy
+    {
+        public ComplementaryStrategy()
+        {
+        }
+
+        private bool _isColorsChanging;
+
+        public override void ChangeColorPoints(IList<ColorPoint> colorPoints)
+        {
+            _isColorsChanging = true;
+            try
+            {
+                var primaryColorPoint = colorPoints.FirstOrDefault(p => p.IsPrimary);
+                if (primaryColorPoint == null)
+                    return;
+
+                var primaryHsv = primaryColorPoint.Color.ToHsv();
+                primaryHsv.S = 0.76;
+                primaryHsv.V = 1;
+                var primatyIndex = colorPoints.IndexOf(primaryColorPoint);
+                for (int i = 0; i < colorPoints.Count; i++)
+                {
+                    if (i == primatyIndex)
+                        continue;
+
+                    var hue = primaryHsv.H;
+                    var colorPoint = colorPoints[i];
+                    if (i > primatyIndex)
+                    {
+                        hue += 180;
+                        if (hue > 360)
+                            hue -= 360;
+                    }
+                    var value = primaryHsv.V;
+                    var saturation = primaryHsv.S;
+                    if (Math.Abs(i - primatyIndex) > 1)
+                    {
+                        value -= (Math.Abs(i - primatyIndex) - 1) * 0.3;
+                        value = Math.Max(0, value);
+                    }
+
+                    saturation *= 1 + Math.Abs(i - primatyIndex) * 0.15;
+                    saturation = Math.Min(1, saturation);
+
+                    colorPoint.Color = Microsoft.Toolkit.Uwp.Helpers.ColorHelper.FromHsv(hue, saturation, value);
+                }
+            }
+            finally
+            {
+                _isColorsChanging = false;
+            }
+        }
+
+        public override void OnColorChanged(ColorPoint colorPoint, Color oldColor, IList<ColorPoint> colorPoints)
+        {
+            base.OnColorChanged(colorPoint, oldColor, colorPoints);
+            if (_isColorsChanging)
+                return;
+
+            _isColorsChanging = true;
+            try
+            {
+                var primaryColorPoint = colorPoints.FirstOrDefault(p => p.IsPrimary);
+                if (primaryColorPoint == null)
+                    return;
+
+                var primaryHsv = primaryColorPoint.Color.ToHsv();
+                var primatyIndex = colorPoints.IndexOf(primaryColorPoint);
+                var colorPointHsv = colorPoint.Color.ToHsv();
+                var colorPointIndex = colorPoints.IndexOf(colorPoint);
+                if (primaryColorPoint != colorPoint)
+                {
+                    if (colorPointIndex > primatyIndex)
+                        primaryHsv.H = colorPointHsv.H;
+                    else
+                    {
+                        primaryHsv.H = colorPointHsv.H + 180;
+                        if (primaryHsv.H > 360)
+                            primaryHsv.H -= 360;
+                    }
+                }
+
+                for (int i = 0; i < colorPoints.Count; i++)
+                {
+
+                    var hue = primaryHsv.H;
+                    var point = colorPoints[i];
+                    var pointHsv = point.Color.ToHsv();
+                    if (i > primatyIndex)
+                    {
+                        hue += 180;
+                        if (hue > 360)
+                            hue -= 360;
+                    }
+
+                    var saturationRate = primaryHsv.S / pointHsv.S;
+                    if (saturationRate == 1 )
+                    {
+                        saturationRate = 1 + Math.Abs(i - primatyIndex) * 0.15;
+                    }
+                    var saturation = pointHsv.S * saturationRate;
+                    point.Color = Microsoft.Toolkit.Uwp.Helpers.ColorHelper.FromHsv(hue, saturation, pointHsv.V);
+                }
+            }
+            finally
+            {
+                _isColorsChanging = false;
+            }
+
+        }
+    }
+}
