e9c4ab297c5cf2d7cb56fa129fb50b56f5142cf6
Dino Chan <dino.chan@live.com>
2017-12-06 17:02:42 +0800

    Add DoubleN2ThumbToolTipValueConverter.

---
+++ ColorfulBox/ColorfulBox/Converters/DoubleN2ThumbToolTipValueConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace ColorfulBox
+{
+    public class DoubleN2ThumbToolTipValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is double doubleValue && doubleValue < 0)
+                return doubleValue.ToString("N2");
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
